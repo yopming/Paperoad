9314cb695a991caf48574cdc1a619fac816ae6c3
Tieming <yopming@gmail.com>
2023-01-25 14:26:34 -0500

    Initial Commit

---
+++ Paperoad/PaperoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperoadApp.swift
+//  Paperoad
+//
+//  Created by Tieming on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PaperoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
