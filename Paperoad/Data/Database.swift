310c3dcbfd26937e167ab611f2eb74dda728c844
Tieming <yopming@gmail.com>
2023-01-28 21:45:49 -0500

    Revert "add initial tables"
    This reverts commit 1b9dce99c1dc133b6b04063de261744940b49f27.

--- Paperoad/Data/Database.swift
+++ Paperoad/Data/Database.swift
@@ -19,3 +19,47 @@ struct Database {
         writer
     }
 }
+
+// MARK: - Preview
+extension Database {
+    static let preview: Database = {
+        let writer = DatabaseQueue()
+        return try! Database(writer)
+    }()
+}
+
+// MARK: - Migrator
+extension Database {
+    var migrator: DatabaseMigrator {
+        var migrator = DatabaseMigrator()
+        
+        #if DEBUG
+        migrator.eraseDatabaseOnSchemaChange = true
+        #endif
+        
+        migrator.registerMigration("v1") { db in
+        }
+        return migrator
+    }
+    
+}
+
+// MARK: - Persistent
+extension Database {
+    static let persistent: Database = {
+        do {
+            let fileManager = FileManager()
+            let folder = try fileManager
+                .url(
+                    for: .applicationSupportDirectory,
+                    in: .userDomainMask,
+                    appropriateFor: nil,
+                    create: true
+                )
+                .appendingPathComponent("data", isDirectory: true)
+            // BreadBuddy github reddit
+        } catch {
+            fatalError("Unresolved error: \(error)")
+        }
+    }()
+}
