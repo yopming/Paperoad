44db5eb418cb10834f5416f1091b2c91fe67a905
Tieming <yopming@gmail.com>
2023-02-06 16:39:49 -0500

    group add function

--- Paperoad/UI/Sheets/SidebarGroupAddView.swift
+++ Paperoad/UI/Sheets/SidebarGroupAddView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct SidebarGroupAddView: View {
+    @Environment(\.appDatabase) private var appDatabase
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var errorAlertIsPresented = false
+    @State private var errorAlertMessage = ""
+    
     // keep track of if the sheet should be shown
     @Binding var showSheet: SidebarSheetView?
     
@@ -31,9 +37,11 @@ struct SidebarGroupAddView: View {
             HStack {
                 Button("Close", role: .cancel) {
                     showSheet = nil
+                    dismiss()
                 }
                 Button("Create") {
                     showSheet = nil
+                    save(name: groupName)
                 }
                     .disabled(groupName.isEmpty)
                     .buttonStyle(.borderedProminent)
@@ -44,6 +52,17 @@ struct SidebarGroupAddView: View {
         .padding()
         .textFieldStyle(.roundedBorder)
         .frame(width: 350, alignment: .leading)
+    }
+    
+    private func save(name: String) {
+        do {
+            var group = Group(name: name, createTime: Date(), updateTime: Date())
+            try appDatabase.saveGroup(&group)
+            dismiss()
+        } catch {
+            errorAlertIsPresented = true
+            errorAlertMessage = (error as? LocalizedError)?.errorDescription ?? "Error occurred"
+        }
     }
 }
 
